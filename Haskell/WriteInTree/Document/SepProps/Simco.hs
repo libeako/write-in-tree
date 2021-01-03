751342b14d357ba05e2627cf137bb4574134e646
libeako <libeako@earth.milky-way>
2021-01-03 18:59:24 +0100

    Simco: Use the line data directly.
    Drop the usage of the higher level Simco data structures, that were used as intermediary till now.

--- Haskell/WriteInTree/Document/SepProps/Simco.hs
+++ Haskell/WriteInTree/Document/SepProps/Simco.hs
@@ -20,8 +20,6 @@ import qualified Fana.Develop.Test.Define as Test
 import qualified Fana.Math.Algebra.Category.OnTypePairs as Category2
 import qualified Fana.Math.Algebra.Monoid.Accumulate as Accu
 import qualified Fana.Optic.Concrete.Prelude as Optic
-import qualified Fana.Serial.Bidir.Instances.Text.PropertyTree.Simco.DataRender as Simco
-import qualified Fana.Serial.Bidir.Instances.Text.PropertyTree.Simco.Data as Simco
 import qualified Fana.Serial.Bidir.Instances.Text.PropertyTree.Simco.DataLines as SimcoLow -- low level data
 import qualified Fana.Serial.Bidir.Instances.Text.PropertyTree.Simco.Layer as Simco
 import qualified Fana.Serial.Print.Show as Fana
@@ -31,16 +29,9 @@ import qualified WriteInTree.Document.SepProps.Parse as Parse
 type Text = String
 
 -- | renders the given data into simco language.
-to_simco :: DocSepProps -> Simco.Forest
+to_simco :: DocSepProps -> Forest SimcoLow.Node
 to_simco props = 
-	Simco.ItemsStructure
-		[
-			Simco.make_atom "language-version" (Accu.extract (Fana.show (language_version props)))
-		]
-
-to_simco_tree :: DocSepProps -> Forest SimcoLow.Node
-to_simco_tree = to_simco >>> Simco.render
-
+	[Base.Node (SimcoLow.make_atom "language-version" (Accu.extract (Fana.show (language_version props)))) []]
 
 data ParseError
 	= ParseErrorInSimcoLayer Simco.ParseError
@@ -52,7 +43,7 @@ instance Fana.Showable Text ParseError where
 		ParseErrorInUpperLayer details -> "error parsing layer above SimCo : " <> Fana.show details
 
 upper_layer :: Optic.PartialIso' (Accu.Accumulated Text) (Base.Forest SimcoLow.Node) DocSepProps
-upper_layer = Optic.PartialIso to_simco_tree Parse.parse_from_line_forest
+upper_layer = Optic.PartialIso to_simco Parse.parse_from_line_forest
 
 whole_layer :: Optic.PartialIso' ParseError Text DocSepProps
 whole_layer = 
@@ -71,7 +62,7 @@ parse_from_text = Optic.piso_interpret whole_layer
 test_simco_layer :: Test
 test_simco_layer = 
 	Test.single "simco layer"
-		(Optic.test_piso (Category2.empty, Category2.empty) [] [to_simco_tree Default.def] Simco.layer)
+		(Optic.test_piso (Category2.empty, Category2.empty) [] [to_simco Default.def] Simco.layer)
 
 test_upper_layer :: Test
 test_upper_layer = 
