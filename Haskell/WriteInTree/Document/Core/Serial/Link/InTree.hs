56fc29e47998859a42d28d244ed5cbf2970e68bc
libeako <libeako@earth.milky-way>
2022-10-11 14:09:20 +0200

    layer 'link': small clean

--- Haskell/WriteInTree/Document/Core/Serial/Link/InTree.hs
+++ Haskell/WriteInTree/Document/Core/Serial/Link/InTree.hs
@@ -6,7 +6,6 @@ where
 
 import Data.Tree (Tree (..), Forest)
 import Fana.Prelude
-import WriteInTree.Document.Core.Serial.Link.Individual (MetaNodeName (..))
 
 import qualified Fana.Math.Algebra.Monoid.Accumulate as Accu
 import qualified Fana.Optic.Concrete.Prelude as Optic
@@ -20,9 +19,6 @@ import qualified WriteInTree.Document.Core.Serial.RichTextTree.Position as Pos
 type Text = Base.String
 type A = Label.Elem Text -- additional info wrapper
 type Inline = Data.Inline Text
-
-render_MetaNodeName :: MetaNodeName -> Text
-render_MetaNodeName = \case { MnLink -> Individual.meta_node_name }
 
 type ParseError = Pos.Positioned (Accu.Accumulated Text)
 
@@ -58,7 +54,6 @@ parse (Node trunk children) =
 		from_situation (l, rest_of_children) =
 			map (Node (map (flip Data.Inline l) trunk)) (traverse parse rest_of_children)
 		in parse_children children >>= from_situation
-
 
 layer :: Optic.PartialIso' ParseError (Tree (A Text)) (Tree (A Inline))
 layer = Optic.PartialIso render parse
