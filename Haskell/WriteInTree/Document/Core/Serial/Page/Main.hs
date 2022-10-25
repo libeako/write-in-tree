fbb427ee67aaf049d8ff3c431401125f58cd0122
libeako <libeako@earth.milky-way>
2022-10-25 18:28:23 +0200

    paging: include starting new layer construction into module Main

--- Haskell/WriteInTree/Document/Core/Serial/Page/Main.hs
+++ Haskell/WriteInTree/Document/Core/Serial/Page/Main.hs
@@ -15,5 +15,20 @@ module WriteInTree.Document.Core.Serial.Page.Main
 )
 where
 
+import Data.Tree (Tree)
+import Fana.Math.Algebra.Category.ConvertThenCompose ((>**>^))
 import WriteInTree.Document.Core.Serial.Page.Data
 import WriteInTree.Document.Core.Serial.Page.Tree (layer)
+
+import qualified Fana.Math.Algebra.Category.OnTypePairs as Category2
+import qualified Fana.Optic.Concrete.Prelude as Optic
+import qualified WriteInTree.Document.Core.Data as Basic
+import qualified WriteInTree.Document.Core.Serial.Page.BreakStructure as BS
+import qualified WriteInTree.Document.Core.Serial.Page.Count as Count
+
+
+layer' :: Optic.Iso' (Tree (Basic.Node i)) (BS.Page Count.Ordinal (Basic.Node i))
+layer' =
+	Category2.identity
+	>**>^ BS.layer
+	>**>^ Count.layer
