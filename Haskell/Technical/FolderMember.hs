90680e4c6e6f2d3f64ad8e3ee8dca1f184711c2a
libeako <libeako@earth.milky-way>
2022-11-11 19:06:26 +0100

    file IO: introduce 'member_string'

--- Haskell/Technical/FolderMember.hs
+++ Haskell/Technical/FolderMember.hs
@@ -1,28 +1,43 @@
 module Technical.FolderMember
 (
-	Member (..),
+	Member (..), lift_by_piso, member_string,
 )
 where
 
-import Prelude (IO, String)
 import Fana.Prelude
+import Prelude (IO, String)
+import System.FilePath (FilePath, (</>))
 
+import qualified Data.Bifunctor as Bifunctor
+import qualified Fana.Optic.Concrete.Prelude as Optic
 import qualified Prelude as Base
 
-type Path = String
-type FolderPath = Path
 
 data Member d =
 	Member
 	{ memberName :: String
-	, memberWriter :: FolderPath -> d -> IO ()
-	, memberReader :: FolderPath -> IO (Either String d)
+	, memberWriter :: FilePath -> d -> IO ()
+	, memberReader :: FilePath -> IO (Either String d)
 	}
 
-read :: FolderPath -> Member d -> IO (d)
+read :: FilePath -> Member d -> IO (d)
 read folder_path (Member name _ reader) = 
 	let
 		error_result message = Base.error ("could not read '" <> name <> "'\n" <> message)
 		in map (either error_result id) (reader folder_path)
 
+lift_by_piso :: forall l h . Optic.PartialIso' String l h -> Member l -> Member h
+lift_by_piso (Optic.PartialIso render parse) (Member name writer reader) =
+	let
+		prefix_parse_error_message :: String -> String
+		prefix_parse_error_message message = "while parsing " <> name <> ":" <> message
+		prefixed_parse :: l -> Either String h
+		prefixed_parse = parse >>> Bifunctor.first prefix_parse_error_message 
+		in Member name (map (render >>>) writer) (map (map (>>= prefixed_parse)) reader)
 
+member_string :: String -> FilePath -> Member String
+member_string member_name file_name = 
+	let
+		writer = (</> file_name) >>> Base.writeFile
+		reader = (</> file_name) >>> Base.readFile
+		in Member member_name writer (reader >>> map Right)
--- Haskell/WriteInTree/Document/File.hs
+++ Haskell/WriteInTree/Document/File.hs
@@ -6,8 +6,7 @@ where
 
 import Fana.Prelude
 import Prelude (String, IO, FilePath)
-import System.FilePath ((</>))
-import Technical.FolderMember (Member (..))
+import Technical.FolderMember (Member (..), member_string)
 import WriteInTree.Document.Core.Serial.Page.Main (Site)
 import WriteInTree.Document.Main (Document (..))
 import WriteInTree.Document.SepProps.Data (DocSepProps (..))
@@ -19,40 +18,27 @@ import qualified Fana.Optic.Concrete.Prelude as Optic
 import qualified Fana.Serial.Print.Show as Fana
 import qualified Prelude as Base
 import qualified System.Directory as Directory
+import qualified Technical.FolderMember as FolderMember
 import qualified WriteInTree.Document.Core.Serial.Layers as CoreSerial
 import qualified WriteInTree.Document.SepProps.File as SepProps
-import qualified WriteInTree.Document.SepProps.Simco as SepPropsSimco
-
 
 type Text = Base.String
 
 
-sep_props_file_name :: String
-sep_props_file_name = "properties.simco.text"
-
-render_sep_props :: DocSepProps -> String
-render_sep_props = SepPropsSimco.to_simco_text
-
 member_core :: DocSepProps -> Member Site
-member_core sep_props = 
+member_core sep_props =
 	let
-		file_name = "tree.mm"
-		writer :: FilePath -> Site -> IO ()
-		writer folder_path = 
-			let
-				render :: DocSepProps -> Site -> String
-				render config = Optic.down (CoreSerial.layer config)
-				in render sep_props >>> Base.writeFile (folder_path </> file_name)
-		reader :: FilePath -> IO (Either String Site)
-		reader folder_path = 
-			let
-				parse :: DocSepProps -> String -> Either String Site
-				parse config = 
-					id
-					>>> Optic.piso_interpret (CoreSerial.layer config) 
-					>>> Bifunctor.first (Fana.show >>> Acc.extract)
-				in map (parse sep_props) (Base.readFile (folder_path </> file_name))
-		in Member "core content tree" writer reader
+		render :: DocSepProps -> Site -> String
+		render config = Optic.down (CoreSerial.layer config)
+		parse :: DocSepProps -> String -> Either String Site
+		parse config =
+			id
+			>>> Optic.piso_interpret (CoreSerial.layer config) 
+			>>> Bifunctor.first (Fana.show >>> Acc.extract)
+		in 
+			FolderMember.lift_by_piso 
+				(Optic.PartialIso (render sep_props) (parse sep_props))
+				(member_string "core tree content" "tree.mm")
 
 write :: FilePath -> Document -> IO ()
 write address d =
@@ -65,7 +51,7 @@ write address d =
 				memberWriter (member_core sep_props) address (docCore d)
 
 read :: FilePath -> Monad.ExceptT Text IO Document
-read folder_path = 
+read folder_path =
 	do
 		config <- SepProps.read folder_path
 		core <- Monad.ExceptT (memberReader (member_core config) folder_path)
