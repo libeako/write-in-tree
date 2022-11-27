6d3b6f0cd8ea64983f3c9148bc3f05f35ef6a965
libeako <libeako@earth.milky-way>
2022-11-27 20:01:15 +0100

    delete file Path

--- Haskell/WriteInTree/Document/Core/Serial/All.hs
+++ Haskell/WriteInTree/Document/Core/Serial/All.hs
@@ -4,12 +4,12 @@ module WriteInTree.Document.Core.Serial.All
 )
 where
 
-import Data.Tree (Tree)
+import Data.Tree (Forest)
 import Fana.Math.Algebra.Category.ConvertThenCompose ((>**>^))
 import Fana.Math.Algebra.Monoid.Accumulate (Accumulated)
 import Fana.Prelude
 import WriteInTree.Document.Core.Data
-import WriteInTree.Document.Core.Serial.Position (PositionedMb (..))
+import WriteInTree.Document.Core.Serial.Position
 
 import qualified Fana.Math.Algebra.Category.OnTypePairs as Category2
 import qualified Fana.Optic.Concrete.Prelude as Optic
@@ -18,7 +18,6 @@ import qualified Fana.Serial.Print.Show as Fana
 import qualified WriteInTree.Document.Core.Serial.InNodeTextStructure as Mtt
 import qualified WriteInTree.Document.Core.Serial.Link.InTree as Link
 import qualified WriteInTree.Document.Core.Serial.Node as Node
-import qualified WriteInTree.Document.Core.Serial.Path as Path
 import qualified WriteInTree.Document.Core.Serial.Position as Pos
 
 
@@ -29,13 +28,14 @@ meta_text_escape :: Optic.Iso' PageContentBulk PageContentBulk
 meta_text_escape =
 	Optic.lift_iso_by_function (Optic.fn_up text_content_in_page_content_bulk) Mtt.layer_escapee
 
-type LayerTextTree = Optic.PartialIso' (PositionedMb (Accumulated Text)) Text (Tree Text)
+positioning :: Optic.Iso (Forest Text) (Forest (Count, Text)) (Forest Text) (Forest (Positioned Text))
+positioning = Optic.Iso id (map (map (uncurry Positioned)))
 
 serialize :: Optic.PartialIso' (PositionedMb (Accumulated Text)) Text PageContentBulk
 serialize =
 	Category2.identity
 	>**>^ Optic.piso_convert_error show_error Tt.text_tree
-	>**>^ Path.layer
+	>**>^ positioning
 	>**>^ Optic.piso_convert_error Pos.maybefy_positioned Link.serialize
 	>**>^ Node.serialize
 	>**>^ meta_text_escape
