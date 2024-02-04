30530b26031742f44338c5cf5097b522b9e33d09
libeako <libeako@earth.milky-way>
2024-02-04 14:29:57 +0100

    clean main test file

--- TestMain.hs
+++ TestMain.hs
@@ -7,8 +7,6 @@ import Fana.Prelude
 
 import qualified Fana.Develop.Test.Define as Test
 import qualified Fana.Develop.Test.Run as Test
-import qualified WriteInTree.Document.Core.Serial.RichTextTree.Label.TextSplit as TextSplit
-import qualified WriteInTree.Document.SepProps.Simco as SepProps
 
 
 fail :: Test
@@ -16,8 +14,7 @@ fail = Test.single "dummy test" False
 
 all_tests :: Test
 all_tests = Test.bunch "all" 
-	[ SepProps.test
-	, TextSplit.test_layer
+	[
 	]
 
 main :: Sys.IO ()
